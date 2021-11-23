84e54f85aeb9eb0e884af054ec484ac424e19378
Daniel Silva Ferreira Bruno <dsfbstudent@yahoo.com.br>
2021-11-23 16:11:08 -0300

    Add my remaining code about the PEP-3107...

---
+++ News/PEP-3107/addition.py
@@ -0,0 +1,10 @@
+def addition(a: int, b: float) -> float:
+    return a + b
+
+
+print(addition(1, 2.3))
+# 3.3
+
+print(addition(1.2, 4))
+# there will be no errors if the function is called with incorrect argument types.
+# 5.2
