eafe6b844a5b2c50ba2e4e8d1ce9043cf612dc96
Daniel Silva Ferreira Bruno <dsfbstudent@yahoo.com.br>
2023-02-03 12:25:00 -0300

    Adição de outras aulas para a seção de POO do curso PythonFull - Caio Sampaio.

---
+++ PythonFull/mvc/dao.py
@@ -0,0 +1,21 @@
+
+from model import Pessoa
+
+class PessoaDal:
+	@classmethod
+	def salvar(cls, pessoa: Pessoa):
+		with open('pessoas.txt', 'w') as arq:
+			arq.write(pessoa.nome + " " +
+				str(pessoa.idade) +
+				" " + pessoa.cpf)
+
+	@classmethod
+	def ler(cls):
+		nome = 'caio'
+		idade = 20
+		cpf = '10234332'
+
+		return Pessoa(nome, idade, cpf)
+
+p1 = Pessoa('Caio', 20, '13241234123')
+print(PessoaDal.ler().nome)
---
+++ PythonFull/mvc/model.py
@@ -0,0 +1,6 @@
+
+class Pessoa:
+	def __init__(self, nome: str, idade: int, cpf: str):
+		self.nome = nome
+		self.idade = idade
+		self.cpf = cpf
