ff4174dd29bbe6e235fa2e65d9bb5a4f693c8474
dsfb <dsfbstudent@yahoo.com.br>
2023-03-14 18:14:04 -0300

    Atualizando o status dos arquivos de curso de Python da Alura

--- Alura/Formacao - Python e orientacao a objetos/Python comecando com a linguagem/jogos/adivinhacao.py
+++ Alura/Formacao - Python e orientacao a objetos/Python comecando com a linguagem/jogos/adivinhacao.py
@@ -1,29 +1,54 @@
+import random
+
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
-total_de_tentativas = 3
-rodada = 1
+numero_secreto = random.randrange(1, 101)
+total_de_tentativas = 0
+pontos = 1000
 
-while rodada <= total_de_tentativas:
+print("Qual é o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if nivel == 1:
+    total_de_tentativas = 20
+elif nivel == 2:
+    total_de_tentativas = 10
+elif nivel == 3:
+    total_de_tentativas = 5
+
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
     chute_str = input("Digite o seu número: ")
     print("Você digitou: ", chute_str)
     chute = int(chute_str)
 
+    if chute < 1 or chute > 100:
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
     acertou = (chute == numero_secreto)
     maior = (chute > numero_secreto)
     menor = (chute < numero_secreto)
 
     if acertou:
-        print("Você acertou!")
+        print("Você acertou e fez {} pontos!".format(pontos))
+        break
     else:
         if maior:
             print("Você errou! O seu chute foi maior que o número secreto.")
         elif menor:
             print("Você errou! O seu chute foi menor que o número secreto.")
+
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos -= pontos_perdidos
+
+        if rodada == total_de_tentativas:
+            print("O número secreto era {}. Você fez {}".format(numero_secreto, pontos))
 
     rodada += 1
 
---
+++ Alura/Formacao - Python e orientacao a objetos/Python comecando com a linguagem/jogos/formatexamples.py
@@ -0,0 +1,32 @@
+
+
+# Link da documentação Python:
+# https://docs.python.org/3/library/string.html#formatexamples
+
+print("Tentativa {} de {}".format(1, 3))
+
+print("Tentativa {1} de {0}".format(1, 3))
+
+print("R$ {}".format(1.59))
+
+print("R$ {:f}".format(1.59))
+
+print("R$ {:.2f}".format(1.59))
+
+print("R$ {:.2f}".format(1.5))
+
+print("R$ {:.2f}".format(1.5))
+
+print("R$ {:.2f}".format(1234.50))
+
+print("R$ {:7.2f}".format(1234.50))
+
+print("R$ {:7.2f}".format(1.5))
+
+print("R$ {:07.2f}".format(1.5))
+
+print("R$ {:07d}".format(4))
+
+print("Data {:02d}/{:02d}".format(9, 4))
+
+print("Data {:02d}/{:02d}".format(19, 11))
