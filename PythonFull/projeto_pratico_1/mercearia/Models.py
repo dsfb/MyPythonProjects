74e89b7e821813e91105c7f96cab07d9cd53381f
Daniel Silva Ferreira Bruno <dsfbstudent@yahoo.com.br>
2023-08-21 17:33:13 -0300

    Adicao do projeto pratico 1 - Mercearia, no curso: Python Full, do prof. Caio Sampaio.

---
+++ PythonFull/projeto_pratico_1/mercearia/Models.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+
+
+class Categoria:
+    def __init__(self, categoria):
+        self.categoria = categoria
+
+
+class Produtos:
+    def __init__(self, nome, preco, categoria):
+        self.nome = nome
+        self.preco = preco
+        self.categoria = categoria
+
+
+class Estoque:
+    def __init__(self, produto: Produtos, quantidade):
+        self.produto = produto
+        self.quantidade = quantidade
+
+
+class Venda:
+    def __init__(self, itensVendido: Produtos, vendedor, comprador, quantidadeVendida, data = datetime.now()):
+        self.itensVendido = itensVendido
+        self.vendedor = vendedor
+        self.comprador = comprador
+        self.quantidadeVendida = quantidadeVendida
+        self.data = data
+
+
+class Fornecedor:
+    def __init__(self, nome, cnpj, telefone, categoria):
+        self.nome = nome
+        self.cnpj = cnpj
+        self.telefone = telefone
+        self.categoria = categoria
+
+
+class Pessoa:
+    def __init__(self, nome, telefone, cpf, email, endereco):
+        self.nome = nome
+        self.telefone = telefone
+        self.cpf = cpf
+        self.email = email
+        self.endereco = endereco
+
+class Funcionario(Pessoa):
+    def __init__(self, clt, nome, telefone, cpf, email, endereco):
+        self.clt = clt
+        super(Funcionario, self).__init__(nome, telefone, cpf, email, endereco)
