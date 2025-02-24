a4db341e2359cdae6b02975b091f3bf9841a7bf9
dsfb <dsfbstudent@yahoo.com.br>
2025-02-24 18:37:39 -0300

    Adição das aulas: 'Controller categoria #3 e #4 | Alterar e Mostrar', mais os TODOs, para o projeto prático 1 do curso PythonFull - Caio Sampaio.

--- PythonFull/projeto_pratico_1/mercearia/Controller.py
+++ PythonFull/projeto_pratico_1/mercearia/Controller.py
@@ -30,11 +30,50 @@ class ControllerCategoria:
                     break
 
             print("A categoria foi removida com sucesso!")
-
+            #TODO: Colocar sem categoria no estoque.
             with open('categoria.txt', 'w') as arq:
                 for i in x:
                     arq.writelines(i.categoria)
                     arq.writelines('\n')
 
+    def alterarCategoria(self, categoriaAlterar, categoriaAlterada):
+        x = DaoCategoria.ler()
+
+        cat = list(filter(lambda x: x.categoria == categoriaAlterar, x))
+        # Se a categoria a ser alterada já existe!
+        if len(cat):
+            cat1 = list(filter(lambda x: x.categoria == categoriaAlterada, x))
+            # Se a categoria alterada não existe ainda.
+            if len(cat1) == 0:
+                x = list(map(lambda x: Categoria(categoriaAlterada) if x.categoria == categoriaAlterar else x, x))
+
+                print("A alteração foi efetuada com sucesso!")
+                #TODO: Alterar a categoria também do estoque.
+            else:
+                print("A categoria para a qual deseja alterar já existe!")
+        else:
+            print("A categoria que deseja alterar não existe!")
+
+        with open("categoria.txt", "w") as arq:
+            for i in x:
+                arq.writelines(i.categoria)
+                arq.writelines('\n')
+
+
+    def mostrarCategoria(self):
+        categorias = DaoCategoria.ler()
+
+        if len(categorias) == 0:
+            print("Categoria vazia!")
+        else:
+            for i in categorias:
+                print(f'Categoria: {i.categoria}')
+
+
 # a = ControllerCategoria()
 # a.cadastrarCategoria('Frios')
+
+# a = ControllerCategoria()
+# a.removerCategoria("Frutas")
+# a.alterarCategoria("Carnes", "Verduras")
+# a.mostrarCategoria()
